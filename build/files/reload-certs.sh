be747728750957cb39a27412ad9963de0191ba65
stefaan <stefaan@ponnet.com>
2021-11-28 14:14:09 +0100

    check if nginx is running before restarting it

--- build/files/reload-certs.sh
+++ build/files/reload-certs.sh
@@ -5,5 +5,8 @@ date > /tmp/nginx-restarted.txt
 wget -O /etc/nginx/certs/my.ava.do.crt http://iso.ava.do/my.ava.do.crt
 wget -O /etc/nginx/certs/my.ava.do.key  http://iso.ava.do/my.ava.do.key
 
-nginx -s reload
+if [ -e /var/run/nginx.pid ]
+then
+  nginx -s reload
+fi
 
